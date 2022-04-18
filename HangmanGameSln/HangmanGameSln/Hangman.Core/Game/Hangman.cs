417e0cfb0b23f2d7afd3ae7ff9870208000e1c12
Lauren42706 <4270683@myuwc.ac.za>
2022-04-19 00:08:41 +0200

    Added a list and a randomizer, as well as a few variables

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -9,23 +9,51 @@ namespace Hangman.Core.Game
 
         public HangmanGame()
         {
+            //My list of words(should be 20 or more)
+            string[] _list = new string[]
+                { "BURNOUT", "RATCHET", "FREDDY", "CLANK", "CUPHEAD", "MUGMAN", "PAPYRUS", "KNUCKLES"};
+
+            //This will randomize the list
+            int index = new Random().Next(_list.Length);
+            string guessWords = _list[index];
+
+            string charcterGuess = "";
+
+            //Renders the gallows
             _renderer = new GallowsRenderer();
+        }
+
+        public bool isStillAlive()
+        {
+            bool allLimbs = false;
+
+            //if()
+            //{
+            //    return allLimbs;
+            //}
         }
 
         public void Run()
         {
-            _renderer.Render(5, 5, 6);
+            //int limbs = 6;
 
-            Console.SetCursorPosition(0, 13);
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("Your current guess: ");
-            Console.WriteLine("--------------");
-            Console.SetCursorPosition(0, 15);
+            //while (limbs > 0)
+            //{
 
-            Console.ForegroundColor = ConsoleColor.Green;
+            //}
 
-            Console.Write("What is your next guess: ");
-            var nextGuess = Console.ReadLine();
+            //_renderer.Render(5, 5, 6);
+
+            //Console.SetCursorPosition(0, 13);
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.Write("Your current guess: ");
+            //Console.WriteLine("--------------");
+            //Console.SetCursorPosition(0, 15);
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+
+            //Console.Write("What is your next guess: ");
+            //var nextGuess = Console.ReadLine();
         }
 
     }
