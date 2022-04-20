aa76a784c8e6a34b00974c4381a0e28ea0e775d9
Lauren42706 <4270683@myuwc.ac.za>
2022-04-21 01:14:24 +0200

    Simplified the dashed code

--- HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
+++ HangmanGameSln/HangmanGameSln/Hangman.Core/Game/Hangman.cs
@@ -10,10 +10,14 @@ namespace Hangman.Core.Game
         private string _guessWords;
         private int index;
         private char charcterGuess;
-        private char[] dashedWord;
+        private string _dashedWord;
         private int numOfLimbs = 6;
-        private char[] overwritten;
-        private char[] characters;
+        private char[] overWrite;
+
+        public string ReturnDashed()
+        {
+            return _dashedWord;
+        }
 
         public HangmanGame()
         {
@@ -22,55 +26,50 @@ namespace Hangman.Core.Game
                 { "BURNOUT", "RATCHET", "FREDDY", "CLANK", "CUPHEAD", "MUGMAN", "PAPYRUS", "KNUCKLES", "GANONDORF", "SANS",
                  "MARIO", "LUIGI", "KRATOS", "ATREUS"};
 
-            characters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
             //This will randomize the list
             index = new Random().Next(_list.Length);
             _guessWords = _list[index];
 
-            //This converts the string into an character array
-            dashedWord = _guessWords.ToCharArray();
+            //And this converts each character in the string to a dash
+            for (int i = 0; i < _guessWords.Length; i++)
+            {
+                _dashedWord += "+";
+            }
+        }
+
+        public void OverWrite()
+        {
+            overWrite = _dashedWord.ToCharArray();
+
+            for (int i = 0; i < _guessWords.Length; i++)
+            {
+                if (_guessWords[i] == charcterGuess)
+                {
+                    overWrite[i] = charcterGuess;
+                }
+            }
+            _dashedWord = new string(overWrite);
 
             //Renders the gallows
             _renderer = new GallowsRenderer();
         }
 
-        public void OverWrite()
-        {
-            
-
-            for(int i = 0; i < dashedWord.Length; i++)
-            {
-                if (dashedWord[i] == characters.Length)
-                {
-                    overwritten[i] = (char)characters.Length;
-                }
-            }
-        }
-
         public void Run()
         {
-            _renderer.Render(5, 5, 6);
+            //_renderer.Render(5, 5, 6);
 
             while (numOfLimbs > 0)
             {
-                //And this converts each character in the string to a dash
-                for (int i = 0; i < dashedWord.Length; i++)
-                {
-                    dashedWord[i] = '-';
-                }
-
                 Console.SetCursorPosition(0, 13);
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write("Your current guess: ");
-                Console.WriteLine(dashedWord);
+                Console.WriteLine(ReturnDashed());
 
                 Console.SetCursorPosition(0, 15);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("What is your next guess: ");
-                charcterGuess = Convert.ToChar(Console.ReadLine());
-                Char.ToUpper(charcterGuess);
+                string guess = Console.ReadLine();
+                guess = guess.ToUpper();
             }
         }
     }
